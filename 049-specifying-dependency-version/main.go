5a33016bf8ee3302f27a45a37cad2885d26c058d
Todd McLeod <tuddleymc@gmail.com>
2023-04-27 23:43:47 -0400

    persistently, patiently, you are bound to succeed

---
+++ 049-specifying-dependency-version/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+	puppy.From13()
+
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	s3 := puppy.BigBark()
+	s4 := puppy.BigBarks()
+	fmt.Println(s3)
+	fmt.Println(s4)
+
+	// also like this
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.20
 
 require golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53
 
-require github.com/GoesToEleven/puppy v0.0.0-20230428023817-501e211ca7a0
+require github.com/GoesToEleven/puppy v1.3.0
 
 require github.com/GoesToEleven/dog v0.0.0-20230428023317-90bef1c76cb9 // indirect
