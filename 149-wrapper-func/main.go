feeec05b89cd14e91da68a433f1cb0a4c67cf06d
Todd McLeod <tuddleymc@gmail.com>
2023-06-05 15:59:35 -0400

    drop by drop, the bucket gets filled

--- 149-wrapper-func/main.go
+++ 149-wrapper-func/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
 
+func main() {
+	xb, err := readFile("poem.txt")
+	if err != nil {
+		log.Fatalf("error in main in readFile: %s", err)
+	}
+	fmt.Println(xb)
+	fmt.Println(string(xb))
+}
+
+func readFile(fileName string) ([]byte, error) {
+	xb, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("there was an error in readfile: %s", err)
+	}
+	return xb, nil
 }
