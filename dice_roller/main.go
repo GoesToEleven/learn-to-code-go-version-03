57f2ab026e7e038747e8f61cae64bc2b031b8b19
Tim <timothyjohnlawrence@gmail.com>
2024-02-01 21:18:38 +0000

    Refactor to "so generally you'll want to write scoped functions and then call them in main"

--- dice_roller/main.go
+++ dice_roller/main.go
@@ -9,72 +9,71 @@ import (
 	"time"
 )
 
+func parseInput(input string) (int, int, int, error) {
+	diceExpr := regexp.MustCompile(`^(\d+)d(\d+)([+-]\d+)?$`)
+	matches := diceExpr.FindStringSubmatch(input)
+
+	if matches == nil {
+		return 0, 0, 0, fmt.Errorf("invalid input format")
+	}
+
+	numDice, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return 0, 0, 0, err
+	}
+
+	numSides, err := strconv.Atoi(matches[2])
+	if err != nil {
+		return 0, 0, 0, err
+	}
+
+	modifier := 0
+	if len(matches) > 3 {
+		modifier, err = strconv.Atoi(matches[3])
+		if err != nil {
+			return 0, 0, 0, err
+		}
+	}
+
+	return numDice, numSides, modifier, nil
+}
+
+func rollDice(numDice, numSides int, adv, dis *bool) int {
+	total := 0
+	for i := 0; i < numDice; i++ {
+		roll := rand.Intn(numSides) + 1
+		if *adv {
+			roll = max(roll, rand.Intn(numSides)+1)
+		} else if *dis {
+			roll = min(roll, rand.Intn(numSides)+1)
+		}
+		fmt.Printf("Die %d: %d\n", i+1, roll)
+		total += roll
+	}
+	return total
+}
+
 func main() {
-	// Define flags
 	adv := flag.Bool("adv", false, "Roll twice and take the higher")
 	dis := flag.Bool("dis", false, "Roll twice and take the lower")
-
-	// Parse command line arguments
 	flag.Parse()
-
-	// Regular expression to validate input format (XdY[+-]N)
-	diceExpr := regexp.MustCompile(`^(\d+)d(\d+)([+-]\d+)?$`)
 
 	fmt.Println("Enter dice expression ([number of dice]d[size of dice]+/-[modifier] ie:1d20+4): ")
 	var input string
 	fmt.Scanln(&input)
 
-	// Validate input format
-	if !diceExpr.MatchString(input) {
-		fmt.Println("Invalid input format. Please enter XdY[+-]N where X is the number of dice, Y is the number of sides, and N is the optional modifier.")
+	numDice, numSides, modifier, err := parseInput(input)
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
-	// Extract number of dice, sides, and modifier (if present) from input
-	matches := diceExpr.FindStringSubmatch(input)
-	numDice, _ := strconv.Atoi(matches[1])
-	numSides, _ := strconv.Atoi(matches[2])
-	modifier := 0
-	if len(matches) > 3 {
-		modifier, _ = strconv.Atoi(matches[3])
-	}
-
-	// Seed random number generator
-	rand.Seed(int64(time.Now().UnixNano()))
-
-	// Initialize total
-	var total int
-
-	// Check if adv or dis flag was set
-	if *adv {
-		// Roll twice and take the higher
-		roll1 := rand.Intn(numSides) + 1
-		roll2 := rand.Intn(numSides) + 1
-		highestRoll := max(roll1, roll2)
-		fmt.Printf("Die 1: %d\n", highestRoll)
-		total += highestRoll
-	} else if *dis {
-		// Roll twice and take the lower
-		roll1 := rand.Intn(numSides) + 1
-		roll2 := rand.Intn(numSides) + 1
-		lowestRoll := min(roll1, roll2)
-		fmt.Printf("Die 1: %d\n", lowestRoll)
-		total += lowestRoll
-	} else {
-		// Regular dice rolling
-		for i := 0; i < numDice; i++ {
-			roll := rand.Intn(numSides) + 1
-			fmt.Printf("Die %d: %d\n", i+1, roll)
-			total += roll
-		}
-	}
-
-	// Apply modifier and print total
+	rand.Seed(time.Now().UnixNano())
+	total := rollDice(numDice, numSides, adv, dis)
 	total += modifier
 	fmt.Printf("Total: %d (including modifier of %d)\n", total, modifier)
 }
 
-// Helper functions to find the maximum and minimum of two integers
 func max(a, b int) int {
 	if a > b {
 		return a
